9188b44ed9628e6d9379bc725b8b740d0c10e26d
David Momanyi <ddavidmmomanyi8818@gmail.com>
2025-10-13 23:03:00 +0300

    Add profile photos

--- src/sections/ProfileSection.tsx
+++ src/sections/ProfileSection.tsx
@@ -11,14 +11,14 @@ interface Profile {
 }
 
 const profiles: Profile[] = [
-  { image: '/images/avatar1.jpg', profileName: 'John', profileMessage: 'Hello there', time: '08:00 AM', unread: 0 },
-  { image: '/images/avatar2.jpg', profileName: 'Brian', profileMessage: 'How are you', time: '08:00 AM', unread: 2 },
-  { image: '/images/avatar3.jpg', profileName: 'Mercy', profileMessage: 'Good morning', time: '08:00 AM', unread: 2 },
-  { image: '/images/avatar4.jpg', profileName: 'Arnold', profileMessage: 'Heyyy', time: '08:00 AM', unread: 0 },
-  { image: '/images/avatar5.jpg', profileName: 'Linus', profileMessage: 'How is it going', time: '08:00 AM', unread: 2 },
-  { image: '/images/avatar6.jpg', profileName: 'David', profileMessage: 'It went okay', time: '08:00 AM', unread: 0 },
-  { image: '/images/avatar7.jpg', profileName: 'Joe', profileMessage: 'google.com', time: '08:00 AM', unread: 2 },
-  { image: '/images/avatar8.jpg', profileName: 'Evans', profileMessage: 'Hello there', time: '08:00 AM', unread: 0 },
+  { image: "/src/assets/profile/Iron Man (2).jpg", profileName: 'John', profileMessage: 'Hello there', time: '08:00 AM', unread: 0 },
+  { image: '/src/assets/profile/wp11920705-nubia-red-magic-8-pro-wallpapers-1.png', profileName: 'Brian', profileMessage: 'How are you', time: '08:00 AM', unread: 2 },
+  { image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Mercy', profileMessage: 'Good morning', time: '08:00 AM', unread: 2 },
+  { image: '/src/assets/profile/wallpaperflare.com_wallpaper (1).jpg', profileName: 'Arnold', profileMessage: 'Heyyy', time: '08:00 AM', unread: 0 },
+  { image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Linus', profileMessage: 'How is it going', time: '08:00 AM', unread: 2 },
+  { image: '/src/assets/profile/leonardo.jpg', profileName: 'David', profileMessage: 'It went okay', time: '08:00 AM', unread: 0 },
+  { image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Joe', profileMessage: 'google.com', time: '08:00 AM', unread: 2 },
+  { image: '/src/assets/profile/20240601_171616.jpg', profileName: 'Evans', profileMessage: 'Hello there', time: '08:00 AM', unread: 0 },
 ]
 
 const ProfileSection:React.FC = () => {
