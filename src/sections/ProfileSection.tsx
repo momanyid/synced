146ced6fc6c10dbe66e083afd172fe16dbebd3dd
David Momanyi <ddavidmmomanyi8818@gmail.com>
2025-10-14 00:33:33 +0300

    fix(profile section): Images to have relative url

--- src/sections/ProfileSection.tsx
+++ src/sections/ProfileSection.tsx
@@ -13,14 +13,14 @@ interface Profile {
 }
 
 const profiles: Profile[] = [
-  { id: 1, image: "/src/assets/profile/Iron Man (2).jpg", profileName: 'John', profileMessage: 'Hello there', time: '08:00am'},
-  { id: 2, image: '/src/assets/profile/wp11920705-nubia-red-magic-8-pro-wallpapers-1.png', profileName: 'Brian', profileMessage: 'How are you', time: '08:00am', unread: 2 },
-  { id: 3, image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Mercy', profileMessage: 'Good morning', time: '08:00am', unread: 2 },
-  { id: 4, image: '/src/assets/profile/wallpaperflare.com_wallpaper (1).jpg', profileName: 'Arnold', profileMessage: 'Heyyy', time: '08:00am'},
-  { id: 5, image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Linus', profileMessage: 'How is it going', time: '08:00am', unread: 2 },
-  { id: 6, image: '/src/assets/profile/leonardo.jpg', profileName: 'David', profileMessage: 'It went okay', time: '08:00am' },
-  { id: 7, image: '/src/assets/profile/Iron Man (2).jpg', profileName: 'Joe', profileMessage: 'google.com', time: '08:00am', unread: 2 },
-  { id: 8, image: '/src/assets/profile/20240601_171616.jpg', profileName: 'Evans', profileMessage: 'Hello there', time: '08:00am' },
+  { id: 1, image: "../src/assets/profile/Iron Man (2).jpg", profileName: 'John', profileMessage: 'Hello there', time: '08:00am'},
+  { id: 2, image: '../src/assets/profile/wp11920705-nubia-red-magic-8-pro-wallpapers-1.png', profileName: 'Brian', profileMessage: 'How are you', time: '08:00am', unread: 2 },
+  { id: 3, image: '../src/assets/profile/Iron Man (2).jpg', profileName: 'Mercy', profileMessage: 'Good morning', time: '08:00am', unread: 2 },
+  { id: 4, image: '../src/assets/profile/wallpaperflare.com_wallpaper (1).jpg', profileName: 'Arnold', profileMessage: 'Heyyy', time: '08:00am'},
+  { id: 5, image: '../src/assets/profile/Iron Man (2).jpg', profileName: 'Linus', profileMessage: 'How is it going', time: '08:00am', unread: 2 },
+  { id: 6, image: '../src/assets/profile/leonardo.jpg', profileName: 'David', profileMessage: 'It went okay', time: '08:00am' },
+  { id: 7, image: '../src/assets/profile/Iron Man (2).jpg', profileName: 'Joe', profileMessage: 'google.com', time: '08:00am', unread: 2 },
+  { id: 8, image: '../src/assets/profile/20240601_171616.jpg', profileName: 'Evans', profileMessage: 'Hello there', time: '08:00am' },
 ]
 
 interface Props {
